b83e4c5438395ddb94111b0599971e4c93c9b770
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-09-07 15:21:59 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,25 @@
+const date = document.querySelector(".realtime__day")
+const time = document.querySelector(".realtime__time")
+const getCurrentTime = () => {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
 
+  const newDate = new Date()
+  const currentDay = weekday[newDate.getDay()]
+  const currentTime = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`
+  const millisec = newDate.getTime()
+  date.textContent = currentDay
+
+  time.textContent = millisec
+  // console.log(currentTime)
+}
+
+getCurrentTime()
+setInterval(getCurrentTime, 1)
